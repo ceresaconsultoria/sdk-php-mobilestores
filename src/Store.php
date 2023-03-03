0005c3bf9d7a0b61fe1a3f34bc1ae6bf9f8ae5f6
Weslley Ceresa <ceresaconsultoria@gmail.com>
2023-03-03 10:01:51 -0300

    novas endpoints da loja

--- src/Store.php
+++ src/Store.php
@@ -8,18 +8,122 @@
 
 namespace MobileStores;
 
+use Exception;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+use MobileStores\Core\MSController;
+use MobileStores\Core\MSEventDispatcher;
+use MobileStores\Events\TokenExpired;
+use MobileStores\Exceptions\MSException;
+use MobileStores\Exceptions\MSTokenException;
+
 /**
  * Description of Store
  *
  * @author weslley
  */
-class Store extends Core\MSController{
+class Store extends MSController{
+    
+    public function listPaymentMethods(array $filters = []){
+        if($this->getToken()->expired()){
+            $eventTokenExpired = new TokenExpired(null);
+            
+            MSEventDispatcher::getDispatcher()->dispatch($eventTokenExpired, TokenExpired::NAME);
+            
+            throw new MSTokenException("Token expirado", 1);
+        }
+        
+        try{
+            $response = $this->http->get('api/v1/payment-method', array(
+                "headers" => [
+                    "Authorization" => $this->getToken()->getToken_type() . " " . $this->getToken()->getAccess_token()
+                ],
+                "query" => $filters
+            ));
+
+            $body = (string)$response->getBody();
+                        
+            $bodyDecoded = @json_decode($body);
+            
+            if(!is_object($bodyDecoded))
+                throw new Exception("Server not reponse JSON, response: " . $body);
+
+            return $bodyDecoded->data;
+            
+        } catch (ServerException $ex) {
+            
+            $this->exceptionProcess($ex);
+            
+        } catch (ClientException $ex) {
+            
+            $this->exceptionProcess($ex);
+            
+        } catch (BadResponseException $ex) {
+            
+            $this->exceptionProcess($ex);
+            
+        } catch (Exception $ex) {
+                 
+            $this->checkTokenExpired($ex->getMessage());
+            
+            throw new MSException($ex);
+        
+        }
+    }
+    
+    public function listShippingMethods(array $filters = []){
+        if($this->getToken()->expired()){
+            $eventTokenExpired = new TokenExpired(null);
+            
+            MSEventDispatcher::getDispatcher()->dispatch($eventTokenExpired, TokenExpired::NAME);
+            
+            throw new MSTokenException("Token expirado", 1);
+        }
+        
+        try{
+            $response = $this->http->get('api/v1/shipping-method', array(
+                "headers" => [
+                    "Authorization" => $this->getToken()->getToken_type() . " " . $this->getToken()->getAccess_token()
+                ],
+                "query" => $filters
+            ));
+
+            $body = (string)$response->getBody();
+                        
+            $bodyDecoded = @json_decode($body);
+            
+            if(!is_object($bodyDecoded))
+                throw new Exception("Server not reponse JSON, response: " . $body);
+
+            return $bodyDecoded->data;
+            
+        } catch (ServerException $ex) {
+            
+            $this->exceptionProcess($ex);
+            
+        } catch (ClientException $ex) {
+            
+            $this->exceptionProcess($ex);
+            
+        } catch (BadResponseException $ex) {
+            
+            $this->exceptionProcess($ex);
+            
+        } catch (Exception $ex) {
+                 
+            $this->checkTokenExpired($ex->getMessage());
+            
+            throw new MSException($ex);
+        
+        }
+    }
     
     public function details(){
         if($this->getToken()->expired()){
-            $eventTokenExpired = new Events\TokenExpired(null);
+            $eventTokenExpired = new TokenExpired(null);
             
-            Core\MSEventDispatcher::getDispatcher()->dispatch($eventTokenExpired, Events\TokenExpired::NAME);
+            MSEventDispatcher::getDispatcher()->dispatch($eventTokenExpired, TokenExpired::NAME);
             
             throw new MSTokenException("Token expirado", 1);
         }
