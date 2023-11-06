5c888257da13165d13b4717bc55123cb5efe8ff7
Weslley <ceresaconsultoria@gmail.com>
2023-11-06 16:46:09 -0300

    criado endpoint para pegar os detalhes do produto

--- src/Catalog.php
+++ src/Catalog.php
@@ -1551,4 +1551,51 @@ class Catalog extends MSController{
         }
         
     }
+    
+    public function getProduct($id){
+        if($this->getToken()->expired()){
+            $eventTokenExpired = new Events\TokenExpired(null);
+            
+            Core\MSEventDispatcher::getDispatcher()->dispatch($eventTokenExpired, Events\TokenExpired::NAME);
+            
+            throw new MSTokenException("Token expirado", 1);
+        }
+        
+        try{
+            $response = $this->http->get("api/v1/product/".$id, array(
+                "headers" => [
+                    "Authorization" => $this->getToken()->getToken_type() . " " . $this->getToken()->getAccess_token()
+                ],
+            ));
+
+            $body = (string)$response->getBody();
+                        
+            $bodyDecoded = @json_decode($body);
+            
+            if(!is_object($bodyDecoded))
+                throw new Exception("Server not reponse JSON, response: " . $body);
+                        
+            return $bodyDecoded->data;
+            
+        } catch (ServerException $ex) {
+            
+            $this->exceptionProcess($ex);
+            
+        } catch (ClientException $ex) {
+            
+            $this->exceptionProcess($ex);
+            
+        } catch (BadResponseException $ex) {
+            
+            $this->exceptionProcess($ex);
+            
+        } catch (Exception $ex) {
+                 
+            $this->checkTokenExpired($ex->getMessage());
+            
+            throw new MSException($ex);
+        
+        }
+        
+    }
 }
